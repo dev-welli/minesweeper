11910720251ad148b57581fce3c187ba03143ff9
dev-welli <welli.abdullah@xero.com>
2018-03-25 09:28:35 +1300

    New Game button

--- minesweeper-old.js
+++ minesweeper-old.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-
+ /* Example of dot notation
+ var board = {}
+ board.cells=[
+   {row: 0, col: 0, isMine: true, hidden: true}, {row: 0, col: 1, isMine: true, hidden: true}, {row: 0, col: 2, isMine: true, hidden: true},
+   {row: 1, col: 0, isMine: true, hidden: true}, {row: 1, col: 1, isMine: true, hidden: true}, {row: 1, col: 2, isMine: true, hidden: true},
+   {row: 2, col: 0, isMine: true, hidden: true}, {row: 2, col: 1, isMine: true, hidden: true}, {row: 2, col: 2, isMine: true, hidden: true}
+  ]
+*/
+  //Example of bracket notation
   var board = {
     cells: [
       {row: 0, col: 0, isMine: false, isMarked: false, hidden: true}, {row: 0, col: 1, isMine: true, isMarked: false, hidden: true}, 
@@ -25,14 +33,16 @@ function startGame () {
   lib.initBoard()
 }
 
-//refresh the webpage to re-initialize the board
-function restart (){
-  document.location.reload()
-}
-
 
 // Define this function to look for a win condition:
+//
+// 1. Are all of the cells that are NOT mines visible?
+// 2. Are all of the mines marked?
 function checkForWin () {
+
+  // You can use this function call to declare a winner (once you've
+  // detected that they've won, that is!)
+  //   lib.displayMessage('You win!')
 
   for (var i = 0; i < board.cells.length; i++){//checking the status of each cells
     if (board.cells[i].isMine){
@@ -45,12 +55,19 @@ function checkForWin () {
     }
   }
 
-  //if all mines have been marked and no cells are hidden, game is won
+  //if all mines have been marked and no cells are hidden, game is over
   lib.displayMessage('You win!')
 
 }
 
 // Define this function to count the number of mines around the cell
+// (there could be as many as 8). You don't have to get the surrounding
+// cells yourself! Just use `lib.getSurroundingCells`: 
+//
+//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+//
+// It will return cell objects in an array. You should loop through 
+// them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
   var surrounding = lib.getSurroundingCells(cell.row, cell.col);
 
